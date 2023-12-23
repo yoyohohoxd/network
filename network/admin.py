8e5fd038dba5a5f652ce03a3190d7c115ab501ad
yoyohohoxd_WSL <madse@live.dk>
2023-12-23 15:30:45 +0100

    Don't know, small modification in profile.html

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, NewPost, Profile
+
+admin.site.register(User)
+admin.site.register(NewPost)
+admin.site.register(Profile)
